dd3331ccd9204ef19f92036cec020eeb7644e21d
Raghu Betina <rbetina@gmail.com>
2012-07-25 22:05:39 -0500

    Created app, supported url '/posts', reading in data from Graph API.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,14 @@
+require 'open-uri'
+
+class PostsController < ApplicationController
+  def index
+    # This will expire every hour or so. You'll have to get a new one from the Graph API explorer.
+    my_access_token = ""
+    
+    url = "https://graph.facebook.com/me/home?access_token=#{my_access_token}&limit=200"
+    
+    @response = open(url).read
+    
+    @results = JSON.parse(@response)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendBC::Application.config.secret_token = 'd01f6db0d290a2706aa00d52de89d099214bac1a797406ff82d74bac2a5200294cc16b2888920824fd07641e1589ab0be47592990e2961cf8659b5f380f9e40f'
