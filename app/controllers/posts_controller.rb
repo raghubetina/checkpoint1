22727a046583899dfb131ed7e98666e073ec04b3
Raghu Betina <rbetina@gmail.com>
2012-07-25 22:59:26 -0500

    Clean-up.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,14 +3,16 @@ require 'open-uri'
 class PostsController < ApplicationController
   def index
     # This will expire every hour or so. You'll have to get a new one from the Graph API explorer.
-    my_access_token = ""
+    my_access_token = "AAACEdEose0cBAAplzSJNMIfEsdSGR3ZC43ytvZBngYo2DYyIq1gaPNmq5upBYj6phaVGAsjAKA0ZB85Lg9GG4LeVNuJDTsgDLSuzIUINwZDZD"
     
     url = "https://graph.facebook.com/me/home?access_token=#{my_access_token}&limit=200"
     
-    @response = open(url).read
+    response = open(url).read
     
-    @results = JSON.parse(@response)
+    json_response = JSON.parse(response)
     
-    @posts = @results["data"]
+    @posts = json_response["data"]
+    
+    
   end
 end
